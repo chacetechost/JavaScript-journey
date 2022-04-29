cb958ffcae7db61fb86943b77c732c346053734b
Wayne Chace <realtinuijioluwa1@gmail.com>
2022-04-29 18:43:08 +0100

    playlist

---
+++ exercise1.js
@@ -0,0 +1,25 @@
+// First class, We will be learning commenting.
+
+// There are two types of commenting,
+// BUt before we proceed, I will tell you what a comment is.
+
+// A comment or commenting means, writing plain notes in the workspace and indirectly telling
+// the computer to ignore the plain text and run the codes.
+// This is useful when you want to write something in the code for
+// Your team mates or yourself when you want to
+// Remember what something does.
+
+// Notice how i am using a double slash befor writing this texts?
+// That's me commanding the computer to ignore it.
+// and this is the first way for you to comment. Now on to the second way of commenting.
+
+//  It would be stressful to have to put slashes
+//  before any text no matter how long the lines
+
+// this is why you should use a / asterics and another asterics with a closing slash.
+
+/* This is an example of writing comments on multiple lines without having to put
+slashes on every line. Now when you run this in the console. you will see nothing, Because
+we have simple told the computer to ignore it. 
+I am teaching you this first cause we will be using a lot of this 
+through out the class.  */
---
+++ exercise2.js
@@ -0,0 +1,7 @@
+// Printing out things in the console, No matter what it is,
+// once there is a console.log before the command,
+// It will be printed as a string. Examples are as follows.
+console.log('Hello world, how are you?');
+
+// When you run this in your google chrome dev console.
+// You will get Hello world, How are you?
