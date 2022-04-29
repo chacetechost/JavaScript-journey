032aabc7a1bca6e59f1cf0c02a0cc123a5f87603
Wayne Chace <realtinuijioluwa1@gmail.com>
2022-04-29 18:43:37 +0100

    .

---
+++ exercise5.js
@@ -0,0 +1,30 @@
+/* DATA TYPES IN JS 
+We will be working with the lists of data types we have. 
+i will explain each of them as simple as i can. we have: 
+Strings
+Numbers
+Boolean
+Undefined
+null
+symbol. this is not commonly used, But like i said, 
+No knowledge is a waste. so i will explain all. */
+
+// these dataTypes work mostly with variables. for example.
+
+const name = 'Chace'; // The quotes like i said in previous lessons symbolizes a string.
+const age = 34; // Without the quotes now, it prints the value of the number varaibel. for you.
+const rating = 4.5; // this will ignore the decimal point, i will explain why shortly.
+const isCool = true; // This seems to be self explanatory, You are just gibing a command to something if its true or false.
+const x = null; // This simply means something is empty, the data of x is empty. thats why we have null.
+const y = undefined; // This means a variable isn't defined. for instance we do not have something stored in y
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+
+// This will give you the data types of eachof these variables.
+// undefined and null are actually errors, because they must always take in a value.
---
+++ exercise6.js
@@ -0,0 +1,5 @@
+// Numbers in JavaScript. This is how you increase and decrease numbers in
+// JavaScript, The = and - operators.
+var num1 = 10;
+num1--;
+console.log(num1);
