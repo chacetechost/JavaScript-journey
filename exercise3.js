fb493f7c4565ff134d89b0b913e9f830fd78b849
Wayne Chace <realtinuijioluwa1@gmail.com>
2022-04-29 18:43:23 +0100

    .

---
+++ exercise3.js
@@ -0,0 +1,10 @@
+/* To alert means to flash a message. on your browser. Its like a mini notification at the top of your browser
+for instance chrome. That pop up you get when you try to submit a form after a bad network, That is an alert message 
+ powered by Javascript...  */
+
+// alert('Yoo, This is a warming. Go back now!');
+
+// This will continue to buzz you
+// Untill you comment it out, You will rarely use this
+// In a real project, but it is always good to know it
+// as No knowledge is a waste.
---
+++ exercise4.js
@@ -0,0 +1,22 @@
+/* ---------------------------Variables.-------------------- */
+
+/* What are Variables? 
+ Picture a piggy bank, where you keep your money, 
+You have a piggy bank, and you have a little hole where you poke your money through
+like a box, This is a Variable, a store for words or texts which you will
+use predominantly in your codes, It saves you the stress
+pf having to write it everytime. so all you just need to do is 
+write either of these three command 'var', 'let', 'const' and give it any name, then you 
+console log it. it will give you the value for that name. example below */
+
+var b = 'Smoothie';
+console.log(b);
+
+// You will get Smoothie in the console. cause Smoothie is stored in 'b'.
+
+// another example for Numbers.
+
+const someNumber = 45;
+console.log(someNumber);
+
+// You will get 45 in the console when you run this.
