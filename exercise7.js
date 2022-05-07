cc680e0d996c2a137e440cfd6427d84540225345
Wayne Chace <realtinuijioluwa1@gmail.com>
2022-05-07 22:50:27 +0100

    .

---
+++ exercise7.js
@@ -0,0 +1,9 @@
+// Functions are things you will work with a lot in JavaScript, i believe
+// all languages have thier functions.  I will explain extensively soon enough.
+
+function fun() {
+  console.log('This is a function');
+}
+
+// This is how you call a function.
+fun();
